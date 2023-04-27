98ef72166445cee5e3c4dbc538055f569b617673
dolivon123456 <dolivon555@gmail.com>
2023-04-27 21:59:35 +0100

    get value

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
-""" Add type annotations to safely_get_value looking at the output"""
-from typing import Any, Mapping, TypeVar, Union
+"""Contains method that safely gets value from dictionary."""
+from typing import Mapping, Any, Union, TypeVar, Optional
 
 T = TypeVar(T)
 
 
 def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[T, None] = None
-                     ) -> Union[Any, T]:
-    """" Duck typing """
+                     default: Optional[T] = None) -> Union[Any, T]:
+    """Safely gets value from dictionary.
+    Args:
+        dct (dict): Dictionary to get value from.
+        key (str): Key to get value from.
+        default (any): Default value to return if key is not found.
+    Returns:
+        Value from dictionary.
+    """
     if key in dct:
         return dct[key]
     else:
