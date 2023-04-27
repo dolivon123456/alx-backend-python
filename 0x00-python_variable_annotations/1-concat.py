c7db9f7826abcb3fcb4330570d82a7edc0ce2b4c
dolivon123456 <dolivon555@gmail.com>
2023-04-27 20:37:45 +0100

    concat str1+str2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+This module supplies the annonated function `concat`.
+
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """concat str1 and str2 and returns a concatenated string"""
+    return str1 + str2#!/usr/bin/env python3
+
+"""
+This module supplies the annonated function `concat`.
+
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """concat str1 and str2 and returns a concatenated string"""
+    return str1 + str2
